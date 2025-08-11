fb3c78219400ffd6d41ac8b2af9f3fc2acb986d1
Giovanna <gg.aventureira@gmail.com>
2025-08-11 20:57:57 -0300

    Lampada

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,11 @@
+import EffectCounter from "@/components/EffectCounter";
+import Lampada from "@/components/Lampada";
 import { LikeButton } from "@/components/LikeButton";
 
 export default function HomePage() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <LikeButton />
+      <Lampada />
     </div>
   );
 }
