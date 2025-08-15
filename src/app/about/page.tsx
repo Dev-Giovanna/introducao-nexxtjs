5e2d51634254a1ca7a039038caf289c8c75415d0
Giovanna <gg.aventureira@gmail.com>
2025-08-14 21:52:56 -0300

    Calculadora

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,12 @@
 import EffectCounter from "@/components/EffectCounter";
 import Lampada from "@/components/Lampada";
 import { LikeButton } from "@/components/LikeButton";
+import MiniDisplay from "@/components/miniDisplay";
 
 export default function HomePage() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <Lampada />
+    <div className="flex justify-center items-center h-screen w-full">
+      <MiniDisplay />
       
     </div>
   );
